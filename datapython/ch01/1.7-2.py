3d7754b78d1ed218a2194633806ed266e51e01b7
simseong <a63043457@gmail.com>
2024-11-27 18:26:34 +0900

    Add files via upload

---
+++ datapython/ch01/1.7-2.py
@@ -0,0 +1,12 @@
+Python 3.12.4 | packaged by Anaconda, Inc. | (main, Jun 18 2024, 15:03:56) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle as t
+... t.forward(100)
+... t.left(120)
+... t.forward(100)
+... t.left(120)
+... t.forward(100)
+... t.right(120)
+... t.forward(100)
+... t.right(120)
+... t.forward(100)
